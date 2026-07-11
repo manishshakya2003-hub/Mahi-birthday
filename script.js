d24627423f7b3e46a7335b12ba72ec1672561854
manishshakya2003-hub <manishshakya2003@gmail.com>
2026-07-11 05:46:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-function openGift() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("main").style.display = "block";
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
+function cutCake() {
+    const flame = document.getElementById('candleFlame');
+    const text = document.getElementById('cakeText');
+    
+    flame.style.display = 'none'; // Candle blown
+    
+    text.innerHTML = "🎂 Cake Cut Successfully! Yayyy! ❤️";
+    text.style.color = "#e06c75";
+    text.style.fontWeight = "bold";
 }
-
-function createHeart() {
-  const heart = document.createElement("div");
-  heart.className = "heart";
-  heart.innerHTML = "💖";
-
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = (20 + Math.random() * 30) + "px";
-  heart.style.animationDuration = (5 + Math.random() * 5) + "s";
-
-  document.body.appendChild(heart);
-
-  setTimeout(() => {
-    heart.remove();
-  }, 9000);
-}
-
-setInterval(createHeart, 350);
